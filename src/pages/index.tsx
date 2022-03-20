ac3f0ed9685d7b1b58118578f55be06552b7b40d
webmiraclepro <webmiraclepro@gmail.com>
2022-03-21 07:52:10 +0800

    initial connected removed && blog fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
-import { useState, useEffect } from 'react'
-
-import Web3 from 'web3'
-import { Web3Exception } from '../interface/web3'
 import NavBar from '../components/Navbar';
 import RoadMap from '../components/RoadMap';
 import Strategy from '../components/Strategy';
@@ -12,29 +8,7 @@ import Articles from '../components/Articles';
 import AboutUs from '../components/AboutUs';
 import ContactInfo from '../components/ContactInfo/ContactInfo';
 
-declare let window: any
-
 const Home = () => {
-  const [web3, setWeb3] = useState<Web3 | null>(null)
-  const [address, setAddress] = useState<string>('')
-
-  useEffect(() => {
-    if (!window.ethereum) {
-      console.error('Please install MetaMask')
-      return
-    }
-
-    window.ethereum
-      .request({ method: 'eth_requestAccounts' })
-      .then((accounts: string[]) => {
-        setAddress(accounts[0])
-        let w3 = new Web3(window.ethereum)
-        setWeb3(w3)
-      })
-      .catch((err: Web3Exception) => console.log(err.code))
-  }, [])
-
-
   return (
     <div>
       <NavBar />
