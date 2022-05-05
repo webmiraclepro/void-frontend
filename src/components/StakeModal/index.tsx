bc7acbdaeb49b34f8cfc90cbb36ca76c0e0a17bb
webmiraclepro <webmiraclepro@gmail.com>
2022-05-06 07:23:40 +0800

    modified styles for react-modal

--- src/components/StakeModal/index.tsx
+++ src/components/StakeModal/index.tsx
@@ -1,6 +1,7 @@
 import { useState, useCallback, useEffect } from 'react'
 import StakeCard from "../StakeCard"
 import ReactModal from 'react-modal';
+const close_icon = '/svg/close_icon.svg';
 
 interface ModalProps {
   tokenIds: Array<{ token_id: string }> | undefined,
@@ -11,12 +12,26 @@ interface ModalProps {
 }
 
 const customStyles = {
+  overlay: {
+    // position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.75)'
+  },
   content: {
     top: '50%',
     left: '50%',
     right: 'auto',
     bottom: 'auto',
     marginRight: '-50%',
+    border: '1px solid #ccc',
+    background: '#fff',
+    overflow: 'auto',
+    borderRadius: '4px',
+    outline: 'none',
+    padding: '20px',
     transform: 'translate(-50%, -50%)',
   },
 };
@@ -49,15 +64,17 @@ const StakeModal = ({ tokenIds, isStake, isUnStake, isClaim, closeModal }: Modal
   return (
     <ReactModal
       isOpen={isStake ? isStake : isUnStake ? isUnStake : isClaim ? isClaim : false}
-      onRequestClose={closeModal}
+      // onRequestClose={closeModal}
       style={customStyles}
       ariaHideApp={false}
     >
-      <div className='flex flex-row items-center justify-between my-[10px]'>
-        <h2 className="">NFT!</h2>
-        <button onClick={closeModal}>x</button>
+      <div className='flex flex-row items-center justify-between my-2.5'>
+        <div className="font-bold text-xxl">NFT!</div>
+        <button onClick={closeModal}>
+          <img className="w-[30px] h-auto cursor-pointer" src={close_icon} alt="closeIcon" />
+        </button>
       </div>
-      <div className='flex flex-col space-y-4 justify-center'>
+      <div className='flex flex-col w-1/2 h-1/2 space-y-4 justify-center'>
         {
           tokenIds?.map((k: any) => {
             return (
